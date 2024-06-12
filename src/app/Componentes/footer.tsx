aaacef98d4100082ff989380bf997723a2da5d9d
TonyBarrantes <tonybarrarojas09@gmail.com>
2024-06-12 17:36:40 -0600

    Se agrego el footer

--- src/app/Componentes/footer.tsx
+++ src/app/Componentes/footer.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import Link from 'next/link';
 
-const DefaultComponent = () => {
+const Footer = () => {
     return (
-        <div>
-            <h1>Footer Section</h1>
-        </div>
+        <footer className="footer">
+            <Link href="tel:+1234567890" legacyBehavior>
+                <a>Teléfono</a>
+            </Link> |
+            <Link href="https://instagram.com/Tony_barra09" legacyBehavior>
+                <a target="_blank">Instagram</a>
+            </Link> |
+            <Link href="https://facebook.com/Tony_barra09" legacyBehavior>
+                <a>Facebook</a>
+            </Link> |
+            <Link href="https://twitter.com/Tony_barra09" legacyBehavior>
+                <a target="_blank">Twitter</a>
+            </Link>
+        </footer>
     );
 };
 
-export default DefaultComponent;
+export default Footer;
+
